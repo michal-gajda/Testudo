6a19a736752b741736a44807fda0633e2936e3b2
Michal Gajda <michal@gajda.co.uk>
2026-03-23 13:26:42 +0000

    Init

---
+++ src/Contracts/Interfaces/IService1.cs
@@ -0,0 +1,16 @@
+﻿namespace Testudo.Contracts.Interfaces
+{
+    using System.ServiceModel;
+    using Testudo.Contracts.Models;
+
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+    }
+}
